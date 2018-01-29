6bb6e72012e4468a11dc1690a9b6ead536d2e19d
Hamed.Sharif <hamed_shereef@yahoo.com>
2018-01-30 00:57:49 +0100

    chang color&size

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+
+    document.getElementById("paragraph").innerHTML = "Hello World"
+    document.getElementById("paragraph").style.color="blue"
+    document.getElementById("paragraph").style.fontSize="33px"
+
+}
+
